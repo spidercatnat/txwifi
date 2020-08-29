94688f2d02c1a645eb80a1f44194f640d6adc61a
spidercatnat <bloomjazzer@gmail.com>
2020-08-29 10:06:22 -0700

    latest dockerfile

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/bhoriuchi/go-bunyan/bunyan"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/txn2/txwifi/iotwifi"
+	"github.com/spidercatnat/txwifi/iotwifi"
 )
 
 // ApiReturn structures a message for returned API calls.
